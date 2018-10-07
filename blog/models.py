3084698a58eecb5bada8c245bf618543f025c288
@amrit1207! <jainamrit233@gmail.com>
2018-10-07 10:05:14 +0530

    requirements are added

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,5 @@ class Blog(models.Model):
         return self.title
     def summary(self):
         return self.body[0:100]
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- portfolio2/urls.py
+++ portfolio2/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',jobs.views.add,name='home'),
     path('blog/',include('blog.urls')),
+
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
