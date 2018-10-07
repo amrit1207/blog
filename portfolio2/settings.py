dbd2852e9b33ca3000c72bae420d2b0281869a78
@amrit1207! <jainamrit233@gmail.com>
2018-10-07 12:05:37 +0530

    ready now

--- portfolio2/settings.py
+++ portfolio2/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
 
     }
 }
-db_from_env_url=dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+db_from_venv=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_venv)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
